521d8594bae2e551fed426606b803f46d345e333
Administrator <Administrator@admin-PC>
2015-09-02 09:34:47 +0900

    by office

--- src/main/java/com/youngjee/springprjboard/controller/BController.java
+++ src/main/java/com/youngjee/springprjboard/controller/BController.java
@@ -5,9 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.youngjee.springprjboard.command.BCommand;
+import com.youngjee.springprjboard.command.BContentCommand;
 import com.youngjee.springprjboard.command.BListCommand;
+import com.youngjee.springprjboard.command.BModifyCommand;
+import com.youngjee.springprjboard.command.BWriteCommand;
 
 @Controller
 public class BController {
@@ -32,7 +36,39 @@ public class BController {
 	
 	@RequestMapping("/write")
 	public String write(HttpServletRequest request, Model model) {
-		return "";
+		model.addAttribute("request", request);
+		command = new BWriteCommand();
+		command.execute(model);
+		
+		return "redirect:list";
+	}
+	
+	@RequestMapping("/content_view")
+	public String content_view(HttpServletRequest request, Model model) {
+		
+		model.addAttribute("request", request);
+		command = new BContentCommand();
+		command.execute(model);
+		
+		return "content_view";
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/modify")
+	public String modify(HttpServletRequest request, Model model) {
+		
+		model.addAttribute("request", request);
+		command = new BModifyCommand();
+		command.execute(model);
+		
+		return "redirect:list";
+	}
+	
+	@RequestMapping("/reply_view")
+	public String reply_view(HttpServletRequest request, Model model) {
+		
+		model.addAttribute("request", request)
+		
+		return "reply_view";
 	}
 
 }
